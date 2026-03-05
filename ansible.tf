1d787bb77587af4c3bcf7c9c11062700a583b006
Root <94565470+DragonDamage@users.noreply.github.com>
2026-03-05 13:07:57 +0300

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -1,8 +1,35 @@
-resource "local_file" "hosts_cfg" {
-  content = templatefile("${path.module}/hosts.tftpl",
-   {webservers =  yandex_compute_instance.web
-    databases = yandex_compute_instance.for_each
-    storage = [yandex_compute_instance.storage]}
-  )
-  filename = "${abspath(path.module)}/hosts.cfg"
+locals {
+  web_hosts = [for instance in yandex_compute_instance.web : {
+    name = instance.name
+    ip   = instance.network_interface[0].nat_ip_address
+    fqdn = instance.fqdn # Используем атрибут fqdn
+  }]
+
+  db_hosts = [for key, instance in yandex_compute_instance.db : {
+    name = instance.name
+    ip   = instance.network_interface[0].nat_ip_address
+    fqdn = instance.fqdn
+  }]
+
+  storage_hosts = [{
+    name = yandex_compute_instance.storage_vm.name
+    ip   = yandex_compute_instance.storage_vm.network_interface[0].nat_ip_address
+    fqdn = yandex_compute_instance.storage_vm.fqdn
+  }]
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "ansible-inventory.ini"
+  
+  content = templatefile("${path.module}/ansible-inventory.tpl", {
+    webservers = local.web_hosts,
+    databases  = local.db_hosts,
+    storage    = local.storage_hosts
+  })
+  
+  depends_on = [
+    yandex_compute_instance.web,
+    yandex_compute_instance.db,
+    yandex_compute_instance.storage_vm
+  ]
 }
